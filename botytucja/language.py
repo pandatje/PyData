e4b6596f8ebd2eea067b43f11a6789deeeeff021
Maciej Gryka <maciej.gryka@gmail.com>
2017-12-10 13:05:13 +0100

    remove comments and also output similarity

--- botytucja/language.py
+++ botytucja/language.py
@@ -22,30 +22,21 @@ def find_most_similar(text):
     articles = list(articles.values())
     gen_docs = tokenize_articles(articles)
     dictionary = gensim.corpora.Dictionary(gen_docs)
-    # for i in range(len(dictionary)):
-    #     print(i, dictionary[i])
 
     corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
-    # for doc in corpus:
-    #     print(doc)
-    #     for token, count in doc:
-    #         print(dictionary[token], count)
 
     tf_idf = gensim.models.TfidfModel(corpus)
-    # print(tf_idf)
 
     sims = gensim.similarities.Similarity('.', tf_idf[corpus], num_features=len(dictionary))
-    # print(sims)
-    # print(type(sims))
 
     query_doc = [w.lower() for w in word_tokenize(text)]
     query_doc_bow = dictionary.doc2bow(query_doc)
     query_doc_tf_idf = tf_idf[query_doc_bow]
     most_similar_index = np.argmax(sims[query_doc_tf_idf])
-    return articles[most_similar_index]
+    return np.max(sims[query_doc_tf_idf]), articles[most_similar_index]
 
 
 if __name__ == '__main__':
     query = sys.argv[1]
-    result = find_most_similar(query)
-    print(result)
+    similarity, result = find_most_similar(query)
+    print(similarity, result)
